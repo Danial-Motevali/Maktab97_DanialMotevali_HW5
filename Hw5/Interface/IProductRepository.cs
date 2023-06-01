ff642509e97dcdf5576026875b5f62e25adf90ef
Danial Motevali <danial.motevali.82@gmail.com>
2023-06-01 14:02:16 +0330

    Addproduct Interface

--- Hw5/Interface/IProductRepository.cs
+++ Hw5/Interface/IProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hw5.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace Hw5.Interface
 {
     public interface IProductRepository
     {
+        string AddProduct(Product product);
+
     }
 }
