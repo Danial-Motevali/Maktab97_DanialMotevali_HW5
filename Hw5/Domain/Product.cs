feea51207134362917fd5ee9166f924a1f38fb44
Danial Motevali <danial.motevali.82@gmail.com>
2023-06-01 22:43:19 +0330

    adding constractor to product

--- Hw5/Domain/Product.cs
+++ Hw5/Domain/Product.cs
@@ -11,5 +11,11 @@ namespace Hw5.Domain
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public int Barcode { get; set; }
+        public Product(int productId = 0, string name = "null", int barcode = 0)
+        {
+            ProductId = productId;
+            ProductName = name;
+            Barcode = barcode;
+        }
     }
 }
