a4ad520e6887bed64298b38fd3049e41e3b3abdd
Danial Motevali <danial.motevali.82@gmail.com>
2023-06-01 21:48:54 +0330

    method for serializObject created

--- Hw5/Interface/ProductRepository.cs
+++ Hw5/Interface/ProductRepository.cs
@@ -1,8 +1,10 @@
 ﻿using Hw5.Domain;
+using Hw5.servisec;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Hw5.Interface
@@ -11,7 +13,29 @@ namespace Hw5.Interface
     {
         public string AddProduct(Product product)
         {
-            throw new NotImplementedException();
+            bool status = CheckProductName(product.ProductName);
+
+            if (status)
+            {
+
+            }
+            return "not valid name";
+        }
+        public bool CheckProductName (string productName)
+        {
+            string regexPattern = "[A-Z][a-z]{3,5}[_][0-9]{3}";
+
+            bool status = Regex.IsMatch(productName, regexPattern);
+
+            if (status)
+            {
+                return true;
+            }
+            return false;
+        }
+        public int GiveProductId ()
+        {
+            
         }
 
         public string GetProductById(int id)
---
+++ Hw5/servisec/Json.cs
@@ -0,0 +1,44 @@
+﻿using Hw5.Domain;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Hw5.servisec
+{
+    public static class Json
+    {
+        private static string pathToProduct = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+        private static string pathToStock = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+        public static string SerializeObject(object obj, string fileName)
+        {
+            string input = fileName.ToLower();
+            string path;
+
+            var jsonToFile = JsonConvert.SerializeObject(obj);
+
+            if (input == "product")
+            {
+                path = pathToProduct;
+
+            }else if(input == "stock")
+            {
+                path = pathToStock;
+            }
+            else //need workd!!!
+            {
+                throw new Exception("not valid!");
+            }
+
+            File.AppendAllText(path, jsonToFile);
+
+            return $"file succesfully adde to {type}";
+        }
+        
+    }
+}
