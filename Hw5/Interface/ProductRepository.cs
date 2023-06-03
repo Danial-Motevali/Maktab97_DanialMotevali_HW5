29cc5928cd5e1941e7645f9f2ae1c57cc8b9c099
Danial Motevali <danial.motevali.82@gmail.com>
2023-06-04 02:15:25 +0330

    adding the product services

--- Hw5/Interface/ProductRepository.cs
+++ Hw5/Interface/ProductRepository.cs
@@ -30,36 +30,6 @@ namespace Hw5.Interface
             }
             return "not valid name";
         }
-        private bool CheckProductName (string productName)
-        {
-            string regexPattern = "[A-Z][a-z]{3,5}[_][0-9]{3}";
-
-            bool status = Regex.IsMatch(productName, regexPattern);
-
-            if (status)
-            {
-                return true;
-            }
-            return false;
-        }
-        private int GiveProductId ()
-        {
-            int id = 1;
-            var fileTOJson = Json.ProductDeserialize();
-            
-            foreach(var line in fileTOJson)
-            {
-                if(line.ProductId == id)
-                {
-                    id++;
-                }
-                else
-                {
-                    return id;
-                }
-            }
-            return id;
-        }
 
         public string GetProductById(int id)
         {
--- Hw5/servisec/ProductServisec.cs
+++ Hw5/servisec/ProductServisec.cs
@@ -2,12 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Hw5.servisec
 {
-    public class ProductServisec
+    public static class ProductServisec
     {
+        public static bool CheckProductName(string productName)
+        {
+            string regexPattern = "[A-Z][a-z]{3,5}[_][0-9]{3}";
+
+            bool status = Regex.IsMatch(productName, regexPattern);
+
+            if (status)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static int GiveProductId()
+        {
+            int id = 1;
+            var fileTOJson = Json.ProductDeserialize();
+
+            foreach (var line in fileTOJson)
+            {
+                if (line.ProductId == id)
+                {
+                    id++;
+                }
+                else
+                {
+                    return id;
+                }
+            }
+            return id;
+        }
 
     }
 }
